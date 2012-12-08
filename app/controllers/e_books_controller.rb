c97973ced9dbbaf8debc8454f1438195df790dd2
Your Name <nswish@gmail.com>
2012-12-08 13:20:34 +0800

    ...

--- app/controllers/e_books_controller.rb
+++ app/controllers/e_books_controller.rb
@@ -11,24 +11,20 @@ class EBooksController < ApplicationController
   end
 
   # GET /e_books/1
-  # GET /e_books/1.json
   def show
     @e_book = EBook.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @e_book }
     end
   end
 
   # GET /e_books/new
-  # GET /e_books/new.json
   def new
     @e_book = EBook.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @e_book }
     end
   end
 
@@ -38,46 +34,40 @@ class EBooksController < ApplicationController
   end
 
   # POST /e_books
-  # POST /e_books.json
   def create
     @e_book = EBook.new(params[:e_book])
 
     respond_to do |format|
-      if @e_book.save
+      if params[:token]=='zwyxyz' and @e_book.save
         format.html { redirect_to @e_book, notice: 'E book was successfully created.' }
-        format.json { render json: @e_book, status: :created, location: @e_book }
       else
         format.html { render action: "new" }
-        format.json { render json: @e_book.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PUT /e_books/1
-  # PUT /e_books/1.json
   def update
     @e_book = EBook.find(params[:id])
 
     respond_to do |format|
-      if @e_book.update_attributes(params[:e_book])
+      if @params[:token]=='zwyxyz' and e_book.update_attributes(params[:e_book])
         format.html { redirect_to @e_book, notice: 'E book was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @e_book.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /e_books/1
-  # DELETE /e_books/1.json
   def destroy
+=begin
     @e_book = EBook.find(params[:id])
     @e_book.destroy
 
     respond_to do |format|
       format.html { redirect_to e_books_url }
-      format.json { head :no_content }
     end
+=end
   end
 end
