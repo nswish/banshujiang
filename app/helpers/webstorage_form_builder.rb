dda8b922977a998ff24642eac279895d6665c995
nswish <nswish@gmail.com>
2012-12-11 16:21:06 +0800

    ...

--- app/helpers/webstorage_form_builder.rb
+++ app/helpers/webstorage_form_builder.rb
@@ -2,6 +2,7 @@
 class WebstorageFormBuilder < ActionView::Helpers::FormBuilder
 	def webstorage_select(method, options={}, html_options={})
 		return select(method, [
+														["", ""],
 														["华硕网盘", "华硕网盘"],
 														["百度网盘", "百度网盘"]
 													],
--- app/models/e_book.rb
+++ app/models/e_book.rb
@@ -2,13 +2,11 @@ class EBook < ActiveRecord::Base
   attr_accessible :author, :download_name, :download_url, :format, :image_large_file, :image_small, :language, :name, :publish_year, :publisher
 	attr_accessible :download_name_2, :download_url_2
 
-	IMAGE_DIR = '/data_images'
+	IMAGE_DIR = 'data_images'
 
 	def image_large_file=(file_data)
 		unless file_data.blank?
 			@file_data = file_data
-			filename_ext = file_data.original_filename.split('.').last.downcase
-			self.image_large = "#{IMAGE_DIR}/#{id}.#{filename_ext}" 
 		end
 	end
 
@@ -17,11 +15,25 @@ class EBook < ActiveRecord::Base
 	private
 	def save_upload_image_large
 		if @file_data
+			# if self.image_large has value then test the file exist? and delete it
+			unless self.image_large.blank?
+				file_path = "#{ENV['OPENSHIFT_REPO_DIR']}/public/#{self.image_large}"
+				if File::file? file_path
+					File::delete file_path
+				end
+			end
+
+			# write file to dir and link to it
+			filename_ext = @file_data.original_filename.split('.').last.downcase
+			self.image_large = "/#{IMAGE_DIR}/#{id}.#{filename_ext}" 
+
 			file_path = "#{ENV['OPENSHIFT_REPO_DIR']}/public/#{self.image_large}"
 			File.open(file_path, 'wb') do |f|
 				f.write(@file_data.read)
 			end
+
 			@file_data = nil
+			self.save
 		end
 	end
 end
