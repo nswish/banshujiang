5eba865e59d25ef6b854ca7fc37d993125e2c06b
nswish <nswish@gmail.com>
2012-12-18 15:25:02 +0800

    ...

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121217061549) do
+ActiveRecord::Schema.define(:version => 20121218070052) do
 
   create_table "e_books", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(:version => 20121217061549) do
     t.datetime "updated_at",           :null => false
     t.integer  "publish_year"
     t.string   "programming_language"
+  end
+
+  create_table "identities", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   create_table "value_set_bodies", :force => true do |t|
