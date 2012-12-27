aa1756bc7f9e1b73310ef5d27eece376fdf5bf79
nswish <nswish@gmail.com>
2012-12-27 16:02:36 +0800

    ...

--- app/helpers/ebook_form_builder.rb
+++ app/helpers/ebook_form_builder.rb
@@ -1,5 +1,9 @@
 #-*- encoding: utf-8 -*-
 class EbookFormBuilder < ActionView::Helpers::FormBuilder
+  def webstorage_select(method, options={}, html_options={})
+    value_set_select(ActiveSupport::Inflector.pluralize(method), method)
+  end
+
   def category_select(method, options={}, html_options={})
     value_set_select(ActiveSupport::Inflector.pluralize(method), method)
   end
