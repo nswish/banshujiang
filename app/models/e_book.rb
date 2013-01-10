d47c787338c897d838a102bdeeaed75f3bbed6e6
nswish <nswish@gmail.com>
2013-01-10 15:13:02 +0800

    ...

--- app/models/e_book.rb
+++ app/models/e_book.rb
@@ -55,16 +55,6 @@ class EBook < ActiveRecord::Base
         ebook.created_at = item.created_at
         ebook.updated_at = item.updated_at
         ebook.save
-
-        if url and ebook.image_large then
-          require 'net/http'
-          uri = URI url
-          image_uri = uri.scheme+'://'+uri.host+':'+uri.port.to_s+ebook.image_large
-          file_path = "#{ENV['OPENSHIFT_REPO_DIR']}/public/#{ebook.image_large}"
-          File.open(file_path, 'wb') do |f|
-            f.write(Net::HTTP.get URI(image_uri))
-          end
-        end
       end
     end
   end
