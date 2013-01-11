821f343907c8871fbc453f0dc0442fb8a8123a83
nswish <nswish@gmail.com>
2013-01-11 11:38:56 +0800

    ...

--- app/models/e_book.rb
+++ app/models/e_book.rb
@@ -5,7 +5,7 @@ class EBook < ActiveRecord::Base
 
   ### attributes
   attr_accessible :author, :format, :image_large_file, :image_small, :language, :name, :publish_year, :publisher, :programming_language
-  attr_accessible :mobile_development, :operation_system, :database
+  attr_accessible :mobile_development, :database
 
   ### relation
   has_many :webstorage_links
--- db/migrate/20121227043049_add_column_operation_system_to_e_book.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnOperationSystemToEBook < ActiveRecord::Migration
-  def change
-    add_column :e_books, :operation_system, :string
-  end
-end
