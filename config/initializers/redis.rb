d5ea0fe803cd85fd3dc1bcc662aa1b76c98628f6
nswish <nswish@gmail.com>
2015-12-20 17:43:52 +0800

    initialize redis

---
+++ config/initializers/redis.rb
@@ -0,0 +1,7 @@
+require'redis'
+
+host = ENV['OPENSHIFT_APP_DNS'] ? 'pub-redis-15953.us-east-1-3.7.ec2.redislabs.com' : 'localhost'
+port = ENV['OPENSHIFT_APP_DNS'] ? 15953 : 6379
+password = ENV['OPENSHIFT_APP_DNS'] ? 'iz1MegAGA0szrwdx' : ''
+
+Redis.current = Redis.new :host=>host, :port=>port, :password=>password
