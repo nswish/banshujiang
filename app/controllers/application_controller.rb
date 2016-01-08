c03cd46dbc6edec68fd6c6fecc9a0dd7d54ef964
nswish <nswish@gmail.com>
2016-01-08 17:15:54 +0800

    去除域名redirect相关代码,使用dns服务商提供的显性url实现301跳转

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :redirect_to_new_domain
 	before_filter :check_session_expiration
-
-  NEW_DOMAIN = 'www.banshujiang.cn'
 
 	private
 	def check_session_expiration
@@ -14,17 +11,4 @@ class ApplicationController < ActionController::Base
 
 	def require_login
 	end
-
-  # 域名转移
-	def redirect_to_new_domain
-    puts "#{request.method}, #{request.env['SERVER_NAME']}, #{NEW_DOMAIN}"
-    puts (request.env['SERVER_NAME'] =~ Regexp.new(Regexp.escape(NEW_DOMAIN), true)) == nil
-
-    return unless request.method == 'GET'
-
-    if (request.env['SERVER_NAME'] =~ Regexp.new(Regexp.escape(NEW_DOMAIN), true)) == nil
-      puts "http://#{NEW_DOMAIN}#{request.fullpath}"
-      #redirect_to "http://#{NEW_DOMAIN}#{request.fullpath}", :status => :moved_permanently
-    end
-  end
 end
