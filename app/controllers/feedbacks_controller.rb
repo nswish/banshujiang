edc5168286cb7221218eaed4de44a02ad02d00f3
nswish <nswish@gmail.com>
2016-01-12 13:43:11 +0800

    add feedback experimentally

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,14 +1,3 @@
 class FeedbacksController < ApplicationController
-  before_filter :require_login
-
-  def destroy
-    user = User.find session[:user_id]
-
-    feedback = Feedback.find params[:id]
-    if feedback && feedback.user_id == user.id then
-      feedback.delete
-    end
-
-    redirect_to url_for(:controller=>:users, :action=>:new_feedback)
-  end
+  layout 'e_books'
 end
