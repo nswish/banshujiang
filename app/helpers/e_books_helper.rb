f008389654fa3cb74e7d1eef80b3bf383a211284
nswish <nswish@gmail.com>
2016-12-13 17:09:29 +0800

    书籍的文件名称变更，适配baidu网盘下载后的文件名

--- app/helpers/e_books_helper.rb
+++ app/helpers/e_books_helper.rb
@@ -1,6 +1,6 @@
 #-*- encoding:utf-8 -*-
 module EBooksHelper
   def standard_file_name(ebook)
-    "[#{ebook.name.delete ':/*?|'}].#{ebook.publish_year}.#{ebook.language}版"
+    "#{ebook.name.delete ':/*?|,#'}-#{ebook.publish_year}-#{ebook.language}版"
   end
 end
